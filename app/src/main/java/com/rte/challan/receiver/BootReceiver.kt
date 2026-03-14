c7f5621d38852bc2039ec3311d770d35881dbde3
vikash6759ub-creator <vikash6759ub@gmail.com>
2026-03-14 15:58:01 +0530

    Update BootReceiver.kt

--- app/src/main/java/com/rte/challan/receiver/BootReceiver.kt
+++ app/src/main/java/com/rte/challan/receiver/BootReceiver.kt
@@ -9,7 +9,6 @@ class BootReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
-            // Start background service
             val serviceIntent = Intent(context, BackgroundService::class.java)
             context.startService(serviceIntent)
         }
