e4e1c65bcb43182bd9792c27ac0b8a5c096be74f
vikash6759ub-creator <vikash6759ub@gmail.com>
2026-03-17 22:42:21 +0530

    Update UssdHelper.kt

--- app/src/main/java/com/rte/challan/utils/UssdHelper.kt
+++ app/src/main/java/com/rte/challan/utils/UssdHelper.kt
@@ -3,13 +3,23 @@ package com.rte.challan.utils
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.net.URLEncoder
 
 object UssdHelper {
 
     fun dialUssd(context: Context, ussdCode: String) {
-        val intent = Intent(Intent.ACTION_CALL)
-        intent.data = Uri.parse("tel:$ussdCode")
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        context.startActivity(intent)
+        try {
+            // USSD codes mein '#' hota hai, use encode karna zaruri hai (e.g. # -> %23)
+            val encodedCode = ussdCode.replace("#", Uri.encode("#"))
+            
+            val intent = Intent(Intent.ACTION_CALL).apply {
+                data = Uri.parse("tel:$encodedCode")
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            }
+            
+            context.startActivity(intent)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
